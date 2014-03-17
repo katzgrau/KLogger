f8c8918e84a55bb342079117d22aa006de178dac
Dan Horrigan <dan@dhorrigan.com>
2014-03-17 16:44:44 -0400

    Major overhaul.  It is not PSR-3 compatible and the output is prettier.

--- example/example.php
+++ example/example.php
@@ -3,18 +3,19 @@
 # Should log to the same directory as this file
 require dirname(__FILE__).'/../vendor/autoload.php';
 
-$log   = Katzgrau\KLogger\Logger::instance(dirname(__FILE__), Katzgrau\KLogger\Logger::DEBUG);
-$args1 = array('a' => array('b' => 'c'), 'd');
+$obj = new stdClass();
+$obj->foo = 'bar';
+$log   = new Katzgrau\KLogger\Logger(dirname(__FILE__), Psr\Log\LogLevel::DEBUG);
+$args1 = array('a' => array('b' => 'c'), 'd' => null, 'e' => $obj);
 $args2 = NULL;
 
-$log->logInfo('Info Test');
-$log->logNotice('Notice Test');
-$log->logWarn('Warn Test');
-$log->logError('Error Test');
-$log->logFatal('Fatal Test');
-$log->logAlert('Alert Test');
-$log->logCrit('Crit test');
-$log->logEmerg('Emerg Test');
+$log->emergency('Emergency Test');
+$log->alert('Alert Test');
+$log->critical('Critical Test');
+$log->error('Error Test');
+$log->warning('Warning Test');
+$log->notice('Notice Test');
+$log->info('Info Test');
+$log->debug('Debug Test');
 
-$log->logInfo('Testing passing an array or object', $args1);
-$log->logWarn('Testing passing a NULL value', $args2);
+$log->info('Testing passing an array or object', $args1);
